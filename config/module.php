7061d7ff4df1ebc21a06f8be0afb9621966ca498
wangdong <mail@wangdong.io>
2018-07-30 13:36:36 +0800

    增加restful路由支持

--- config/module.php
+++ config/module.php
@@ -14,16 +14,43 @@ return [
 
     // 路由设置
     'router' => function ($router, $module) {
-        $method = strtolower($_SERVER['REQUEST_METHOD']);
-        $route = array_get($module, 'route');
-        $controller = array_get($module, 'controller');
-        $action = camel_case(implode('_', [$method, array_first(array_get($module, 'subfix')) ? : 'index']));
         $middleware = array_get($module, 'composer.extra.laravel-module.middleware', []);
+        $controller = array_get($module, 'controller');
 
-        // 验证控制器中对应方法是否存在，否则模块路由无效
+        // Controller路由
+        $method = strtolower($_SERVER['REQUEST_METHOD']);
+        $action = camel_case(implode('_', [$method, array_first(array_get($module, 'subfix')) ? : 'index']));
         if (method_exists($controller, $action)) {
+            $route = array_get($module, 'route');
             $router->$method($route, ['uses' => "{$controller}@{$action}", 'middleware' => $middleware]);
         }
+
+        // RESTful路由
+        $resource = array_get($module, 'name');
+        $router->group(['prefix' => $resource, 'middleware' => $middleware], function ($router) use ($controller) {
+            if (method_exists($controller, 'index')) {
+                $router->get('/', $controller . '@index');
+            }
+            if (method_exists($controller, 'create')) {
+                $router->get('/create', $controller . '@create');
+            }
+            if (method_exists($controller, 'store')) {
+                $router->post('/', $controller . '@store');
+            }
+            if (method_exists($controller, 'show')) {
+                $router->get('/{id}', $controller . '@show');
+            }
+            if (method_exists($controller, 'edit')) {
+                $router->get('/{id}/edit', $controller . '@edit');
+            }
+            if (method_exists($controller, 'update')) {
+                $router->put('/{id}', $controller . '@update');
+                $router->patch('/{id}', $controller . '@update');
+            }
+            if (method_exists($controller, 'destroy')) {
+                $router->delete('/{id}', $controller . '@destroy');
+            }
+        });
     },
 
     // 复制文件(支持闭包)
