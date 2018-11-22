8b36d50ee73c0f3d234d0e0ffed3f67fd2caaba3
wangdong <mail@wangdong.io>
2018-11-22 09:02:51 +0800

    Update module.php

--- config/module.php
+++ config/module.php
@@ -8,47 +8,6 @@ return [
         'middleware' => [],
     ],
 
-    // 路由设置
-    'router' => function ($router, $module) {
-        $middleware = array_get($module, 'composer.extra.laravel-module.middleware', []);
-        $controller = array_get($module, 'controller');
-
-        // Controller路由
-        $method = strtolower($_SERVER['REQUEST_METHOD']);
-        $action = camel_case(implode('_', [$method, array_first(array_get($module, 'subfix')) ? : 'index']));
-        if (method_exists($controller, $action)) {
-            $route = array_get($module, 'route');
-            $router->$method($route, ['uses' => "{$controller}@{$action}", 'middleware' => $middleware]);
-        }
-
-        // RESTful路由
-        $resource = array_get($module, 'name');
-        $router->group(['prefix' => $resource, 'middleware' => $middleware], function ($router) use ($controller) {
-            if (method_exists($controller, 'index')) {
-                $router->get('/', $controller . '@index');
-            }
-            if (method_exists($controller, 'create')) {
-                $router->get('/create', $controller . '@create');
-            }
-            if (method_exists($controller, 'store')) {
-                $router->post('/', $controller . '@store');
-            }
-            if (method_exists($controller, 'show')) {
-                $router->get('/{id}', $controller . '@show');
-            }
-            if (method_exists($controller, 'edit')) {
-                $router->get('/{id}/edit', $controller . '@edit');
-            }
-            if (method_exists($controller, 'update')) {
-                $router->put('/{id}', $controller . '@update');
-                $router->patch('/{id}', $controller . '@update');
-            }
-            if (method_exists($controller, 'destroy')) {
-                $router->delete('/{id}', $controller . '@destroy');
-            }
-        });
-    },
-
     // 复制文件(支持闭包)
     'publishes' => function () {
         return [
